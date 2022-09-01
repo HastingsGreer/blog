5a8dbb93e84c52aa858f539235f526f0df2d742c
HastingsGreer <hastings.greer@gmail.com>
2022-09-01 00:17:56 +0000

    Deploying to gh-pages from  @ c7ca6cddec47ba4d8aa38077cbdcd35ca25397bc 🚀

--- assets/Reports/code/table.jl
+++ assets/Reports/code/table.jl
@@ -2,8 +2,12 @@
 #hideall
 for file = readdir("Reports")
 	if file[end-2:end] == ".md"
+		s = ""
+		open("Reports/" * file) do f
+			s = readline(f)
+		end
 		println()
-		println("[" * file * "](" * file[1:end-3] * ")")
+		println("[" * file * "](" * file[1:end-3] * ") " * s[12:end])
 		println()
 	end
 end
