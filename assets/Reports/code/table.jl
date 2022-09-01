d77c19165ea25e06e5bf32b54883f8e9f162e925
HastingsGreer <hastings.greer@gmail.com>
2022-09-01 00:37:45 +0000

    Deploying to gh-pages from  @ 07a10b0c4b1ed0d6f70c7f79232f1c3d387c06d7 🚀

--- assets/Reports/code/table.jl
+++ assets/Reports/code/table.jl
@@ -1,6 +1,24 @@
 # This file was generated, do not modify it. # hide
 #hideall
-for file = readdir("Reports")
+reports_paths = readdir("Reports")
+months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"]
+
+function key(str)
+	
+	if !(str[end-1:end] == "md") || str == "todo.md"
+		return (9999, 999, 999)
+	end	
+	month = indexin([str[1:3]], months)[1]
+	year = parse(Int, str[6:9])
+        day = parse(Int, str[4:5])
+
+	return (-year, -month, -day)
+end
+
+sort!(reports_paths, by=key)
+
+
+for file = reports_paths
 	if file[end-2:end] == ".md"
 		s = ""
 		open("Reports/" * file) do f
