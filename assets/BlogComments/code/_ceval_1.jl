1d81aa5aec52b92f628444368c1953daf2861212
HastingsGreer <hastings.greer@gmail.com>
2025-12-08 15:39:20 +0000

    Deploying to gh-pages from  @ be5efa4d1f479812fbb35c3fdb91dd7c77204700 🚀

--- assets/BlogComments/code/_ceval_1.jl
+++ assets/BlogComments/code/_ceval_1.jl
@@ -1,7 +1,13 @@
 # This file was generated, do not modify it. # hide
-#hideall
-using PyCall 
-buf = IOBuffer(); 
-pyimport("sys").stdout = buf; 
-pyimport("sys").output = @OUTPUT 
-pyimport_conda("scipy", "scipy") 
+using PyCall #hide
+buf = IOBuffer(); #hide
+pyimport("sys").stdout = buf; #hide
+pyimport("sys").output = @OUTPUT #hide
+py""" #hide
+import os #hide
+os.system(''' #hide
+curl https://api.github.com/repos/HastingsGreer/blog/issues/2/comments \
+ | grep -e "body\|login"
+''') #hide
+""" #hide
+print(String(take!(buf))) #hide
